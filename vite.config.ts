f9cbea429c7ba644e5b18737896fe26adfde1174
1216108213@qq.com <1216108213@qq.com>
2021-06-16 21:17:35 +0800

    update reademe

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
   base: './', // 设置打包路径
   server: {
     port: 4500, // 设置服务启动端口号
+    host: "127.0.0.1",
     open: true, // 设置服务启动时是否自动打开浏览器
     cors: true // 允许跨域
   }
